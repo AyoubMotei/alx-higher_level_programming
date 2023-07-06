d3055ece977dd3078a8031992973df8601ad3c4e
AyoubMotei <ayoub.motei@gmail.com>
2023-07-06 16:52:47 -0700

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""
-Function to replace some characters with '\n\n'
-"""
-
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after some characters.
-    """
 
-    if type(text) is not str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    tmp = text.replace(".", ".\n\n")
-    tmp = tmp.replace(":", ":\n\n")
-    tmp = tmp.replace("?", "?\n\n")
-    p = tmp.splitlines(True)
-    ls_strip = []
-    for l in p:
-        if l == "\n":
-            ls_strip.append("\n")
-        else:
-            ls_strip.append(l.lstrip())
-    print("".join(ls_strip), end="")
+
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
