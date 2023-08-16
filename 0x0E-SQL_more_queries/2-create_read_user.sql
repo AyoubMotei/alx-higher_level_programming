436bae70b24f2634036cbc8122fd24610162e068
AyoubMotei <ayoub.motei@gmail.com>
2023-08-16 16:58:50 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- Create or modify the user user_0d_2 and database hbtn_0d_2
 -- Description: This script creates the MySQL user 'user_0d_2' with SELECT privilege on the database 'hbtn_0d_2'.
 -- The password for 'user_0d_2' is set to 'user_0d_2_pwd'. If the user or database already exists, the script does not fail.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
