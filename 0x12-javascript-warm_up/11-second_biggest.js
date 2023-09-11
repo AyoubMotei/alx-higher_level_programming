866d89ab272642dae63341ee49a2977f9697ada0
AyoubMotei <ayoub.motei@gmail.com>
2023-09-12 01:37:43 +0200

    Update Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
-}
 
+const secondBiggest = process.argv
+  .slice(2)
+  .map(Number)
+  .sort((a, b) => b - a)[1];
+console.log(secondBiggest === undefined ? 0 : secondBiggest);
 
