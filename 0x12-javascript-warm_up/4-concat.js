7d93244dc2c1925a08f3298ed8094c0db547d905
AyoubMotei <ayoub.motei@gmail.com>
2023-09-12 05:36:54 -0700

    Update Task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-if (!myArgs[0]) {
-    console.log("undefined is undefined");
-}
-else {
-    console.log(myArgs[0], 'is', myArgs[1]);
-}
+const { argv } = require('process');
+console.log(`${argv[2]} is ${argv[3]}`);
+
 
