2b363212c97cc2d67016cea022373ce9408c4472
AyoubMotei <ayoub.motei@gmail.com>
2023-09-17 16:41:03 -0700

    Your commit message here

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
--- Create or modify the user user_0d_1
--- Description: This script creates the MySQL server user 'user_0d_1' with all privileges on the MySQL server.
--- The password for 'user_0d_1' is set to 'user_0d_1_pwd'. If the user already exists, the script does not fail.
-
+-- This script creates the MySQL server user : user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
